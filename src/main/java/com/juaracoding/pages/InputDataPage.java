0f0034885803c2ac991b4107f11e5ab267a0800c
Putri Tarigan <putrivalenzia14@gmail.com>
2023-09-07 12:22:45 +0700

    Update InputDataPage

--- src/main/java/com/juaracoding/pages/InputDataPage.java
+++ src/main/java/com/juaracoding/pages/InputDataPage.java
@@ -1,12 +1,11 @@
 package com.juaracoding.pages;
 
 import com.juaracoding.drivers.DriverSingleton;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.Keys;
-
 
 import static com.juaracoding.drivers.DriverSingleton.delay;
 
@@ -66,8 +65,22 @@ public class InputDataPage {
     WebElement btnCancelUpdate;
     @FindBy(xpath = "//a[@class='btn btn-primary']")
     WebElement btnSubmit;
-    @FindBy(xpath = "//span[normalize-space()='Field Nomor BPJS Harus Diisi!']")
+    @FindBy(xpath = "/html/body/div[5]/div[1]/div/div/div/div[1]/div[1]/div/form/div/div[3]/span")
     WebElement txtBpjsRequired;
+    @FindBy(xpath = "//span[normalize-space()='Field Nomor KTP Harus Diisi!']")
+    WebElement txtKtpRequired;
+    @FindBy(xpath = "//span[normalize-space()='Field Alamat Harus Diisi!']")
+    WebElement txtAlamatRequired;
+    @FindBy(xpath = "//span[normalize-space()='Field Faskes Asal Harus Diisi!']")
+    WebElement txtFaskesAwalRequired;
+    @FindBy(xpath = "//span[normalize-space()='Field Alasan Harus Diisi!']")
+    WebElement txtAlasanRequired;
+    @FindBy(xpath = "//span[normalize-space()='Field Alasan Harus Diisi!']")
+    WebElement txtBeforeImageFile;
+    @FindBy(xpath = "//span[normalize-space()='Field Alasan Harus Diisi!']")
+    WebElement txtAfterImageFile;
+    @FindBy(xpath = "//span[normalize-space()='Field Alasan Harus Diisi!']")
+    WebElement txtTtdImageFile;
 
     // Custom Method For Input Data
     public void clickInput() {
@@ -101,7 +114,7 @@ public class InputDataPage {
 
     public void selectDropDownFaskesTujuan(String faskesTujuan) {
         dropDownFaskesTujuan.click();
-        inputDropDownFaskesTujuan.sendKeys(faskesTujuan+Keys.ENTER);
+        inputDropDownFaskesTujuan.sendKeys(faskesTujuan + Keys.ENTER);
     }
 
     public void enterAlasan(String alasan) {
@@ -121,41 +134,128 @@ public class InputDataPage {
     public void clickBtnEdit() {
         btnEdit.click();
     }
-    public String getTxtEdit(){
-       return btnEdit.getText();
+
+    public String getTxtEdit() {
+        return btnEdit.getText();
     }
-    public String getTxtSimpan(){
-       return btnUpdate.getText();
+
+    public String getTxtSimpan() {
+        return btnUpdate.getText();
     }
-    public String getDataNama(){
-       return name.getText();
+
+    public String getDataNama() {
+        return name.getText();
     }
-    public String getDataBPJS(){
-       return bpjs.getText();
+
+    public String getDataBPJS() {
+        return bpjs.getText();
+    }
+
+    public String getDataKTP() {
+        return ktp.getText();
+    }
+    public String getDataAlamat() {
+        return alamat.getText();
+    }
+    public String getDataKotaKtp() {
+        return dropDownKotaKTP.getText();
+    }
+    public String getDataFaskesAwal() {
+        return faskesAwal.getText();
+    }
+    public String getDataFaskesTujuan() {
+        return dropDownFaskesTujuan.getText();
+    }
+    public String getDataAlasan() {
+        return alasan.getText();
     }
 
     public void clickBtnUpdate() {
         btnUpdate.click();
     }
+
     public void clickBtnCancel() {
         btnCancelUpdate.click();
     }
-    public void clickBtnSubmit(){
+
+    public void clickBtnSubmit() {
         btnSubmit.click();
     }
-    public String getTxtFormatBPJS(){
+
+    public String getTxtFormatBPJS() {
         delay(2);
         String output = "";
         String required = bpjs.getAttribute("pattern");
-        if (required.equals(".{13,13}")){
+        if (required.equals(".{13,13}")) {
             output += "Please Match the Requested Format: 13 to 13 Character";
         }
         System.out.println(output);
         return output;
     }
 
-    public String getTxtBpjsRequired(){
+    public String getTxtFormatKTP() {
+        delay(2);
+        String output = "";
+        String required = bpjs.getAttribute("pattern");
+        if (required.equals(".{16,16}")) {
+            output += "Please Match the Requested Format: 16 to 16 Character";
+        }
+        System.out.println(output);
+        return output;
+    }
+    public void inputAlamat(Integer myInt) {
+        alamat.sendKeys(String.valueOf(myInt));
+    }
+    public void inputFaskesAwal(Integer myInt) {
+        faskesAwal.sendKeys(String.valueOf(myInt));
+    }
+    public void inputAlasan(Integer myInt) {
+        alasan.sendKeys(String.valueOf(myInt));
+    }
+    public String getTxtBpjsRequired() {
         return txtBpjsRequired.getText();
+    }
+    public String getTxtKtpRequired() {
+        return txtKtpRequired.getText();
+    }
+    public String getTxtAlamatRequired() {
+        return txtAlamatRequired.getText();
+    }
+    public String getTxtFaskesAwalRequired() {
+        return txtFaskesAwalRequired.getText();
+    }
+    public String getTxtAlasanRequired() {
+        return txtAlasanRequired.getText();
+    }
+    public String getTxtFormatAlamatIsNotNumber() {
+        String output = "";
+        String isNotNumber = alamat.getAttribute("type");
+        if (!isNotNumber.equals("Number")) {
+            output += "Please input the right address!";
+        }
+        System.out.println(output);
+        return output;
+    }
+    public String getTxtFormatFaskesAwalIsNotNumber() {
+        String output = "";
+        String isNotNumber = faskesAwal.getAttribute("type");
+        if (!isNotNumber.equals("Number")) {
+            output += "Please input the right faskes awal!";
+        }
+        System.out.println(output);
+        return output;
+    }
+    public String getTxtFormatAlasanIsNotNumber() {
+        String output = "";
+        String isNotNumber = alasan.getAttribute("type");
+        if (!isNotNumber.equals("Number")) {
+            output += "Please input the right faskes awal!";
+        }
+        System.out.println(output);
+        return output;
+    }
+    public String getBeforeImageFileName() {
+     return txtBeforeImageFile.getText();
     }
 
 
